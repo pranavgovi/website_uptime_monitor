bb510b415b1c717f0a7085d03ecd777b372ea5c2
Pranav <pranav292005@gmail.com>
2026-01-17 18:54:12 -0500

    Initial Laravel 10 project setup with configuration files

---
+++ backend/config/mail.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+    'default' => env('MAIL_MAILER', 'smtp'),
+
+    'mailers' => [
+        'smtp' => [
+            'transport' => 'smtp',
+            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+            'port' => env('MAIL_PORT', 587),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
+            'timeout' => null,
+            'local_domain' => env('MAIL_EHLO_DOMAIN'),
+        ],
+
+        'ses' => [
+            'transport' => 'ses',
+        ],
+
+        'array' => [
+            'transport' => 'array',
+        ],
+
+        'log' => [
+            'transport' => 'log',
+            'channel' => env('MAIL_LOG_CHANNEL'),
+        ],
+    ],
+
+    'from' => [
+        'address' => env('MAIL_FROM_ADDRESS', 'do-not-reply@example.com'),
+        'name' => env('MAIL_FROM_NAME', env('APP_NAME', 'Example')),
+    ],
+
+];
